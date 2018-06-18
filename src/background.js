35febc5f3e309c4c1e9bc72bf2cbbd2938bd39ed
Daniele Colangelo <me@danielecolangelo.it>
2018-06-18 21:15:07 +0200

    Add temporary icons

--- src/background.js
+++ src/background.js
@@ -2,6 +2,9 @@
 browser.menus.create({
 	id       : 'popup-popup-contextMenus-link',
 	title    : 'Open in a Popup',
+	icons    : {
+		'16' : 'icons/fi-arrows-out.svg'
+	},
 	contexts : ['link'],
 	onclick  : (info,tab) => {
 		browser.windows.create({
