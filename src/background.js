d039fe495eb6a7fb7419b85d86b9ceef1f04748e
Daniele Colangelo <me@danielecolangelo.it>
2018-06-18 22:45:46 +0200

    Add context menu item to move tabs into a popup

--- src/background.js
+++ src/background.js
@@ -15,3 +15,20 @@ browser.menus.create({
 		});
 	},
 });
+
+browser.menus.create({
+	id       : 'popup-popup-contextMenus-tab',
+	title    : 'Move in a Popup',
+	icons    : {
+		'16' : 'icons/fi-arrows-out.svg'
+	},
+	contexts : ['tab'],
+	onclick  : (info,tab) => {
+		browser.windows.create({
+			type   : 'popup',
+			tabId  : tab.id,
+			height : 360,
+			width  : 640
+		});
+	},
+});
