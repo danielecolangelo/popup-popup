3cec5fbb11dc69ef1025de985840526232226cca
Daniele Colangelo <me@danielecolangelo.it>
2018-07-02 18:00:49 +0200

    Simplify browser_action

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -3,9 +3,9 @@ var ppPopup = {
 	 * Declare global vars
 	 */
 	vars : {
-		buttons : document.querySelectorAll('#buttons div.panel-section-tabs-button'),
-		panels : document.querySelectorAll('#panels>div'),
-		lists : document.querySelectorAll('#panels>div>.panel-section-list'),
+		listPopups : document.querySelector('#list-popups'),
+		currentTab : document.querySelector('#current-tab-action'),
+		settingsButton : document.querySelector('#settings-button'),
 		defaultFavicon : browser.extension.getURL('icons/logo.svg'),
 	},
 
@@ -13,90 +13,75 @@ var ppPopup = {
 	 * Functions
 	 */
 	fn : {
-		deselectAll : function () {
-			for (let i = 0; i < ppPopup.vars.buttons.length; i++) {
-				ppPopup.vars.buttons[i].classList.remove('select');
-			}
-			for (let i = 0; i < ppPopup.vars.panels.length; i++) {
-				ppPopup.vars.panels[i].classList.add('hidden');
-			}
-		},
-
-		selectOne : function (e) {
-			let el = e.currentTarget;
-			ppPopup.fn.deselectAll();
-			el.classList.add('select');
-			ppPopup.vars.panels[el.dataset.index].classList.remove('hidden');
-		},
-
 		populateLists : function(){
-			for (let index = 0; index < ppPopup.vars.lists.length; index++) {
+			browser.tabs.query({windowType : 'popup'}).then(tabs => {
+				for (let i = 0; i < tabs.length; i++) {
+					let items = document.createDocumentFragment();
 
-				let dataType = ppPopup.vars.lists[index].dataset.type,
-					dataActionText = ppPopup.vars.lists[index].dataset.actiontext;
-				
-				browser.tabs.query({windowType : dataType})
-					.then( (tabs) => {
-						let items = document.createDocumentFragment();
-						ppPopup.vars.lists[index].innerHTML = '';
+					for (let i = 0; i < tabs.length; i++) {
+						let item = document.createElement('div'),
+							icon = document.createElement('img'),
+							text = document.createElement('div');
 
-						for (let i = 0; i < tabs.length; i++) {
-							let item = document.createElement('div'),
-								icon = document.createElement('img'),
-								text = document.createElement('div');
+						item.classList.add('panel-list-item');
+						icon.classList.add('icon');
+						text.classList.add('text');
 
-							item.classList.add('panel-list-item');
-							icon.classList.add('icon');
-							text.classList.add('text');
+						item.dataset.id = tabs[i].id;
+						item.dataset.actiontext = 'Move to a Window ⮯';
+						icon.setAttribute('src', tabs[i].favIconUrl ? tabs[i].favIconUrl : ppPopup.vars.defaultFavicon);
+						text.innerText = tabs[i].title;
 
-							item.dataset.id = tabs[i].id;
-							item.dataset.type = dataType;
-							item.dataset.actiontext = dataActionText;
+						item.appendChild(icon);
+						item.appendChild(text);
 
-							icon.setAttribute('src', tabs[i].favIconUrl ? tabs[i].favIconUrl : ppPopup.vars.defaultFavicon);
+						item.addEventListener('click', (e) => {
+							let el = e.currentTarget,
+								id = el.dataset.id ? parseInt(el.dataset.id) : 0,
+								promise = pplib.action.toWindow(null,{id: id});
 
-							text.innerText = tabs[i].title;
+							promise.then( (tabs) => {
+								ppPopup.fn.populateLists();
+							})
+						});
 
-							item.addEventListener('click', (e) => {
-								let el = e.currentTarget,
-									id = el.dataset.id ? parseInt(el.dataset.id) : 0,
-									moveTab = 'normal' === el.dataset.type ? pplib.action.toPopup : pplib.action.toWindow,
-									promise = moveTab(null,{id: id});
+						items.appendChild(item);
+					}
 
-								promise.then( (tab) => {
-									ppPopup.fn.populateLists();
-								})
-							});
-
-							item.appendChild(icon);
-							item.appendChild(text);
-
-							items.appendChild(item);
-						}
-
-						ppPopup.vars.lists[index].appendChild(items);
-					} );
-			}
+					ppPopup.vars.listPopups.innerHTML = '';
+					ppPopup.vars.listPopups.appendChild(items);
+				}
+			});
 		},
 	}
 }
 
-
 /**
- * addEventListeners
+ * Settings Button
  */
 
-for (let i = 0; i < ppPopup.vars.buttons.length; i++) {
-	ppPopup.vars.buttons[i].addEventListener('click', ppPopup.fn.selectOne)
-}
-
-
-/**
- * Populate lists
- */
-ppPopup.fn.selectOne({currentTarget : ppPopup.vars.buttons[0]});
-ppPopup.fn.populateLists();
-
-document.querySelector('#settings-button').addEventListener( 'click', (e) => {
+ppPopup.vars.settingsButton.addEventListener( 'click', (e) => {
 	browser.runtime.openOptionsPage();
 });
+
+/**
+ * Current tab button
+ */
+
+ppPopup.vars.currentTab.addEventListener( 'click', (e) => {
+	browser.tabs.query({
+			active : true,
+			currentWindow : true,
+		}).then( tabs => {
+			if( tabs.length > 0 ){
+				console.log(tabs[0])
+				pplib.action.toPopup(null,{id: tabs[0].id})
+			}
+		})
+});
+
+/**
+ * Populate Popups List
+ */
+
+ppPopup.fn.populateLists();
